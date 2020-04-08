aa64df59aaece02a0f5ce864914a9c4cfbe3b748
Laila Romero <lmromero4@miners.utep.edu>
2020-04-07 22:24:30 -0600

    new stateMachine

--- project/src/stateMachine.c
+++ project/src/stateMachine.c
@@ -1,64 +1,7 @@
 #include <msp430.h>
-
 #include "stateMachine.h"
-
 #include "led.h"
-
-
-
-char toggle_red()/* always toggle! */
-
-{
-
-  static char state = 0;
-
-
-
-  switch (state){
-
-  case 0:
-
-    red_on = 1;
-
-    state = 1;
-
-    break;
-
-  case 1:
-
-    red_on = 0;
-
-    state = 0;
-
-    break;
-
-  }
-
-  return 1;/* always changes an led */
-
-}
-
-
-
-char toggle_green()/* only toggle green if red is on!  */
-
-{
-
-  char changed = 0;
-
-  if (red_on) {
-
-    green_on ^= 1;
-
-    changed = 1;
-
-  }
-
-  return changed;
-
-}
-
-
+#include "switches.h"
 
 void state_advance() /* alternate between toggling red & green */
 
@@ -76,6 +19,7 @@ void state_advance() /* alternate between toggling red & green */
     break;
   case 4:
     break;
+  default:
   }
 }
     
