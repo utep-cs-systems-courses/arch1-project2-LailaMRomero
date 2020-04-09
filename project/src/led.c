98f93634bf0a57310b65c982ed9bca9bd2bb5001
Laila Romero <lmromero4@miners.utep.edu>
2020-04-08 20:02:12 -0600

    updated toggle methods

--- project/src/led.c
+++ project/src/led.c
@@ -1,6 +1,13 @@
 #include <msp430.h>
 #include "led.h"
 #include "switches.h"
+#include "stateMachine.h"
+
+unsigned char red_on = 0, green_on = 0;
+unsigned char led_changed = 0;
+
+static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
+
 
 void led_init()
 
@@ -14,13 +21,16 @@ void led_init()
 
 
 void led_update(){
-
   if(switch_state_changed){
+    char ledFlags = LED_RED; //by default, no LEDs on
 
-    char ledFlags = 0; //by default, no LEDs on
     ledFlags |= switch_state_down ? LED_GREEN : LED_RED; //if button is pressed, turn LED Green, otherwise, LED Red
+
     P1OUT &= (0xff ^ LEDS) | ledFlags; //clear bits for off leds
+
     P1OUT |= ledFlags; //set bits for on leds
+
+    
 
   }
 
@@ -33,8 +43,26 @@ void toggle_green(){
   red_on=0;
 }
 
-void toggle_red(){
-  green_on=0;
-  red_on=1;
+
+char toggle_red()/* always toggle! */
+
+{
+
+  static char state = 0;
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state = 1;
+    break;
+
+  case 1:
+    red_on = 0;
+    state = 0;
+    break;
+
+  }
+
+  return 1;/* always changes an led */
+
 }
 
